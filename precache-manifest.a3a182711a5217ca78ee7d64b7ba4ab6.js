c6acd626aa842a1134816f5fe009e6ecdafe5c42
kwoy <gringo.first@gmail.com>
2020-06-09 20:00:30 +0200

    Updates

---
+++ precache-manifest.a3a182711a5217ca78ee7d64b7ba4ab6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2ec30028ca289f62c6d9919f8e4af8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eecf949f31b7a1b50bbc",
+    "url": "/static/css/main.96357396.chunk.css"
+  },
+  {
+    "revision": "7f1638228d471d5e205d",
+    "url": "/static/js/2.16bdf3a7.chunk.js"
+  },
+  {
+    "revision": "44b724887c788dd91af1f626e24324fa",
+    "url": "/static/js/2.16bdf3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eecf949f31b7a1b50bbc",
+    "url": "/static/js/main.71a61169.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "037bbf4a7e678024a8a8cb88745527eb",
+    "url": "/static/media/blog.037bbf4a.jpg"
+  },
+  {
+    "revision": "55bde89e621b73ac8f3926b23ff7b260",
+    "url": "/static/media/bmi.55bde89e.jpg"
+  },
+  {
+    "revision": "3421b8f110eb9e327f76b61689ede056",
+    "url": "/static/media/body_bcg.3421b8f1.jpg"
+  },
+  {
+    "revision": "6c5016223aa459cffef26df966a624af",
+    "url": "/static/media/cup-of-coffee-1280537_1920.6c501622.jpg"
+  },
+  {
+    "revision": "64c60fa5d9d10265dddcb77343c59a8e",
+    "url": "/static/media/soccer.64c60fa5.jpg"
+  },
+  {
+    "revision": "c17f18f45f06f746e935c9da517008c2",
+    "url": "/static/media/todo.c17f18f4.jpg"
+  }
+]);
