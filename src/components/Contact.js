1e80a2de766e72672e529b0786a66633c507fe06
kwojtas172 <k.wojtas172@gmail.com>
2020-08-26 23:19:03 +0200

    section contact - icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,6 +15,20 @@ export default function Contact() {
 
     return (
         <section className="contact" id="contact">
+            <h3 className="contact__tittle">If you want to contact me, please send me message</h3>
+            <article className="contact__info">
+                <ul className="contact__info__list">
+                    <li className="contact__info__list__element">
+                        <i className="contact__info__list__element fab fa-linkedin"></i>
+                    </li>
+                    <li className="contact__info__list__element">
+                        <i className="contact__info__list__element fab fa-github-square"></i>
+                    </li>
+                    <li className="contact__info__list__element">
+                        <i className="contact__info__list__element far fa-envelope"></i>
+                    </li>
+                </ul>
+            </article>
             <form className="contact__form">
                 <input type="text" className={`contact__form__input ${introClass}`} placeholder="Enter your name" />
                 <input type="e-mail" className={`contact__form__input ${introClass}`} placeholder="Enter your e-mail" />
