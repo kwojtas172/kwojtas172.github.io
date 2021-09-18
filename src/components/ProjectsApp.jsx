5c030c7d85536ff3c69e9ff000ae2b1aeaa2c798
kwojtas172 <k.wojtas172@gmail.com>
2021-09-18 14:56:13 +0200

    gh pages & Section of projects

--- src/components/ProjectsApp.jsx
+++ src/components/ProjectsApp.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
 
-
 const Section = styled.section`
     width: 100%;
 `
@@ -9,17 +8,30 @@ const Section = styled.section`
 const Article = styled.article`
     width: 100%;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    position: relative;
+    margin: 1rem 0;
 `
 
 const Photo = styled.img`
-    width: 50%;
+    width: 100%;
     height: auto;
 `
 
 const DescriptionsWrapper = styled.div`
-    width: 50%;
+    width: 100%;
+    display: none;
     flex-direction: column;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 0;
+    right: 0;
+    text-align: center;
+
+    .visible {
+        display: flex;
+    }
 `
 
 const Link = styled.a`
@@ -34,10 +46,35 @@ const ProjectDescription = styled.p`
 const arrOfProjects = [{title: 'Pasieka pod Lasem', description: 'strona o pasiece', url: 'https://pasieka-pod-lasem.pl/', img: '../images/bee-garden.jpg'}, {title: 'Football Scores', description: 'strona o wynikach sportowych', url: 'https://kwojtas172.github.io/scores-app/', img: '../images/football-scores.jpg'}, {title: 'Zaplanuj jedzonko', description: 'strona o planowaniu diety', url: 'https://kwojtas172.github.io/plan-a-diet', img: '../images/plan-a-diet.jpg'}, {title: 'Oddaj rzeczy', description: 'strona o oddawaniu rzeczy', url: 'https://kwojtas172.github.io/share-items', img: '../images/share-items.jpg'}, {title: 'ToDo App', description: 'strona o dodawaniu zadań', url: 'https://kwojtas172.github.io/toDo/', img: '../images/todo.jpg'}, {title: 'BMI Calculator', description: 'strona o obliczaniu BMI', url: 'https://kwojtas172.github.io/BMI-calculator/dist/', img: '../images/bmi-calculator.jpg'}, {title: 'Rzut kostką', description: 'strona o rzucaniu kostką', url: 'https://kwojtas172.github.io/cube-first_project/', img: '../images/cube.jpg'}]
 
 const ProjectsApp = () => {
+
+
+    const showProject = e => {
+        const projectsSection = document.getElementById('projects');
+        const projectsTop = projectsSection.getBoundingClientRect().top
+        if(projectsTop < 120) {
+            const projects = projectsSection.getElementsByTagName('article');
+            const arrOfProjects = Array.prototype.slice.call(projects)
+            arrOfProjects.forEach((project, index) => {
+                if(project.getBoundingClientRect().top < 120) {
+                    console.log(projects[index].getElementsByTagName('div')[0]);
+
+                    projects[index].getElementsByTagName('div')[0].style.display = 'flex'
+                }
+            });
+            if(projects[projects.length-1].getElementsByTagName('div')[0].style.display === 'flex') document.removeEventListener(showProject)
+        }
+    }
+
+
+    React.useEffect(() => {
+        
+        document.addEventListener('scroll', showProject)
+    })
+    
     return (
-        <Section>
-            {arrOfProjects.map(project => {
-                return <Article>
+        <Section id='projects'>
+            {arrOfProjects.map((project, index) => {
+                return <Article key={index} data-id={index}>
                     <Photo alt='' src={project.img} />
                     <DescriptionsWrapper>
                         <Link href={project.url}>{project.title}</Link>
