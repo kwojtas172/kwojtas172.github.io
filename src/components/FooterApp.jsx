943feb1654b1d15df200ddde1c969156997d89d4
kwojtas172 <k.wojtas172@gmail.com>
2021-09-20 15:04:30 +0200

    fonts awesome

--- src/components/FooterApp.jsx
+++ src/components/FooterApp.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import styled from "styled-components";
 
 const Footer = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     width: 100%;
     height: 5rem;
     background-color: grey;
     color: #ffffff;
 `
 
+
 const FooterApp = () => {
     return (
-        <Footer></Footer>
+        <Footer>
+            <i className="fab fa-linkedin-in"></i>
+            <i className="fab fa-github"></i>
+            <i className="far fa-envelope"></i>
+        </Footer>
     );
 }
 
